1d0d3402ae42bd0d478987618746cea375c2ee77
JmlKhan <jmlkhon17@gmail.com>
2022-02-09 15:51:15 +0500

    adding office draft controller

---
+++ TelegramBotAPI/Controllers/OfficeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PresentationLayer.Controllers
+{
+    [Route("api/[controller]")]
+    
+    public class OfficeController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddOffice()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOffice()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
