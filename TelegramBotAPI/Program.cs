a1c96562e0b2151fe94ae7cf6a136122b5e9c85b
JmlKhan <jmlkhon17@gmail.com>
2022-02-09 15:57:33 +0500

    adding swagger

--- TelegramBotAPI/Program.cs
+++ TelegramBotAPI/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
