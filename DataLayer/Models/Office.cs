5eb70efb5afea7a191bd506a1e10fa21dadb5ef1
SMARTSOFT\L.Dudauri <l.dudauri@smartsoft.ge>
2022-02-09 12:47:36 +0400

    add office model

---
+++ DataLayer/Models/Office.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Models
+{
+    class Office
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Adress { get; set; }
+		public int CityId { get; set; }
+		public int CountryId { get; set; }
+		public bool Parking { get; set; }
+	}
+}
