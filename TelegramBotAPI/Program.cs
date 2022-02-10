93f5c24488c3e09e62a918a7ca16158a34da0e8a
JmlKhan <jmlkhon17@gmail.com>
2022-02-10 15:06:36 +0500

    Set up Hangfire

--- TelegramBotAPI/Program.cs
+++ TelegramBotAPI/Program.cs
@@ -1,3 +1,4 @@
+using Hangfire;
 using Telegram.Bot;
 using TelegramBotAPI;
 
@@ -9,7 +10,12 @@ builder.Services.AddHttpClient("tgwebhook")
             .AddTypedClient<ITelegramBotClient>(httpClient =>
             new TelegramBotClient("5120059284:AAEW1xdREZG09BSV5akzkZaifa_nEUJOr48", httpClient));
 
+builder.Services.AddHangfire(x => x.UseSqlServerStorage("<connection string>"));
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
+
+app.UseHangfireDashboard();
 
 app.MapGet("/", () => "Hello World!");
 
