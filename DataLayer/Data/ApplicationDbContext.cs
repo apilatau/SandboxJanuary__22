7db89113104cdbec982f0bd7bc6530d650fad269
Avto <achangelia@gmail.com>
2022-02-10 13:50:08 +0400

    create log model

--- DataLayer/Data/ApplicationDbContext.cs
+++ DataLayer/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace DataLayer.Data
         }
 
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
---
+++ DataLayer/Models/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataLayer.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public DateTime Logtime { get; set; }
+        public int EmployeesId { get; set; }
+        public string EventType { get; set; }
+        public string ObjectName { get; set; }
+        public string ObjectType { get; set; }
+        public string SqlCommand { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+}
