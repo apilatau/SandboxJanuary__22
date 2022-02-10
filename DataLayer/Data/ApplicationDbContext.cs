277c31d0c3af223e43ec4c86499adca340b29a82
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2022-02-10 16:54:34 +0500

    Creating User Model instead Employee model

--- DataLayer/Data/ApplicationDbContext.cs
+++ DataLayer/Data/ApplicationDbContext.cs
@@ -3,20 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer.Data
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class ApplicationDbContext : DbContext
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="options"></param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
 
-        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
