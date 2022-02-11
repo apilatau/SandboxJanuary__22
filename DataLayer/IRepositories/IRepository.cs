abac0f144af6289cef03d2fe74a8ef8a9a51ceb6
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2022-02-11 11:12:07 +0500

    Implementing Repository Functionality

---
+++ DataLayer/IRepositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace DataLayer.IRepositories
+{
+    public interface IRepository<T>
+    {
+        public void Create(T _object);
+
+        public void Update(T _object);
+
+        public IEnumerable<T> GetAll();
+
+        public T GetById(int id);
+
+        public void Delete(T _object);
+    }
+}
