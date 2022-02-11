fe61dae4afcd77f5b83e6fdbfae0ca0d9fa7186d
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2022-02-11 15:09:43 +0500

    Adding IReadRepositoryBase and IRepositoryBase interfaces

---
+++ DataLayer/IRepositories/IReadRepositoryBase.cs
@@ -0,0 +1,25 @@
+﻿namespace DataLayer.IRepositories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IReadRepositoryBase<T> where T : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TId"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<T?> GetById<TId>(TId id, CancellationToken cancellationToken = default) where TId : notnull;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<List<T>> GetAll(CancellationToken cancellationToken = default);
+    }
+}
---
+++ DataLayer/IRepositories/IRepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿namespace DataLayer.IRepositories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepositoryBase<T> : IReadRepositoryBase<T> where T : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<T> Create(T entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task Update(T entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task Delete(T entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<int> SaveChanges(CancellationToken cancellationToken = default);
+    }
+}
