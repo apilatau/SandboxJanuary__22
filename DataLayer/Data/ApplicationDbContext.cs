4189f9008ffe88e3abb84e76d120fb8b953d13cf
Avto <achangelia@gmail.com>
2022-02-15 20:48:04 +0400

    Replace Employee with User

--- DataLayer/Data/ApplicationDbContext.cs
+++ DataLayer/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace DataLayer.Data
 
         }
 
-        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<Log> Logs { get; set; }
     }
 }
---
+++ DataLayer/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public int TelegramId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+        //public Role Role { get; set; }
+
+        public DateTime EmploymentStart { get; set; }
+        public DateTime EmploymentEnd { get; set; }
+
+        public bool IsWorking { get; set; }
+        public bool HaveVacation { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+        //public Country Country { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+        //public City City { get; set; }
+
+    }
+}
