81c5e73800a7ad0e48b2824a3beee3d39f11acf3
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-02-18 02:43:39 -0800

    Added objects to map, report, reserve, workingdesk, for eager loading. reccorect EmployeeId in Report table to UserId

--- DataLayer/Models/Report.cs
+++ DataLayer/Models/Report.cs
@@ -5,10 +5,18 @@ namespace DataLayer.Models
     public class Report
     {
         public int Id { get; set; }
-        public int EmployeeId { get; set; }
+        public int UserId { get; set; }
+        //public User User { get; set; }
+
         public int CountryId { get; set; }
+        //public Country Country { get; set; }
+
         public int CityId { get; set; }
+        //public City City { get; set; }
         public int OfficeId { get; set; }
-        public int ReserveId { get; set; } 
+        //public Office Office { get; set; }
+
+        public int ReserveId { get; set; }
+        //public Reserve Reserve { get; set; }
     }
 }
