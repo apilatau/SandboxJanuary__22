0dc03e564b48edae530439ccde4cce9bc9f47f8c
SMARTSOFT\L.Dudauri <l.dudauri@smartsoft.ge>
2022-02-22 01:07:24 +0400

    Create BookingController

---
+++ TelegramBotAPI/Controllers/BookingController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelegramBotAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        [HttpGet(Name = "GetBookingTypes")]
+        public string[] BookingTypes()
+        {
+            return new string[0];
+        }
+        [HttpGet(Name = "GetServiceTypes")]
+        public string[] ServiceTypes()
+        {
+            return new string[0];
+        }
+        [HttpGet(Name = "BookingCheck")]
+        public bool IsBooked(int reservId)
+        {
+            return true;
+        }
+        [HttpPost(Name = "BookingTime")]
+        public void BookTime(DateTime startDate, DateTime endDate)
+        {
+
+        }
+        [HttpPost(Name = "CancelBook")]
+        public void CancelBook(int reservId)
+        {
+
+        }
+        [HttpGet(Name = "CheckFreeOffices")]
+        public int[] FreeOffices()
+        {
+            return new int[0];
+        }
+        [HttpGet(Name = "CheckParking")]
+        public bool IsParking()
+        {
+            return true;
+        }
+
+    }
+}
