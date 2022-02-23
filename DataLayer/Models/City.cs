1066e69450e87c462eb0dac07c55ace5e9e7434f
khasanboi <k.kholmatov@student.inha.uz>
2022-02-24 00:56:57 +0500

    city and country model added

---
+++ DataLayer/Models/City.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelegramBotAPI.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+        //public bool IsCityCenter { get; set; }
+    }
+}
