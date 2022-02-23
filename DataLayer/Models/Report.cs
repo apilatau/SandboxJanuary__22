918e7581b0643ca6eb64a2c3c6f21efff6a7b812
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-02-23 01:42:53 -0800

    removed comments.

--- DataLayer/Models/Report.cs
+++ DataLayer/Models/Report.cs
@@ -7,11 +7,11 @@ namespace DataLayer.Models
         public int UserId { get; set; }
         public User User { get; set; }
         public int CountryId { get; set; }
-        //public Country Country { get; set; }
+        
         public int CityId { get; set; }
-        //public City City { get; set; }
+        
         public int OfficeId { get; set; }
-        //public Office Office { get; set; }
+        
         public int ReserveId { get; set; }
         public Reserve Reserve { get; set; }
     }
