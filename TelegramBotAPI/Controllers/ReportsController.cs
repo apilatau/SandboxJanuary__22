19c491ae5cd16495cfd94b0b8abd2b667267b66f
Avto <achangelia@gmail.com>
2022-02-24 14:39:39 +0400

    create ReportsController

---
+++ TelegramBotAPI/Controllers/ReportsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelegramBotAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+    }
+}
