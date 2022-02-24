d6a6aa43b61ed9a9726483d7912d47051c51fd0e
JmlKhan <jmlkhon17@gmail.com>
2022-02-24 16:18:07 +0500

    empty methods with required queries based on requirements

--- TelegramBotAPI/Controllers/ReportsController.cs
+++ TelegramBotAPI/Controllers/ReportsController.cs
@@ -8,7 +8,25 @@ namespace TelegramBotAPI.Controllers
     public class ReportsController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> ReportByOffice(int officeId, bool full,DateTime startDate, DateTime finishDate)
+        {
+            return Ok();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> ReportByCity(int cityId, bool full,DateTime startDate, DateTime finishDate)
+        {
+            return Ok();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> ReportByFloor(int officeId, int floorId, bool full,DateTime startDate, DateTime finishDate)
+        {
+            return Ok();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> ReportAllOffices(bool full,DateTime startDate, DateTime finishDate)
         {
             return Ok();
         }
