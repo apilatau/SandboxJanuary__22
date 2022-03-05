56c05ab4c4ea630c2cf928bc432c6f33c4084602
JmlKhan <jmlkhon17@gmail.com>
2022-03-05 14:52:02 +0500

    adding WorkingDeskController and implementing method to get all workingDesks

--- BusinessLayer/WorkingDeskService.cs
+++ BusinessLayer/WorkingDeskService.cs
@@ -1,8 +1,21 @@
 ﻿using BusinessLayer.Interfaces;
+using DataLayer.IRepositories;
+using DataLayer.Models;
 
 namespace BusinessLayer
 {
     public class WorkingDeskService : IWorkingDeskService
     {
+        private readonly IWorkingDeskRepository _workingDeskRepository;
+
+        public WorkingDeskService(IWorkingDeskRepository workingDeskRepository)
+        {
+            _workingDeskRepository = workingDeskRepository;
+        }
+
+        public async Task<List<WorkingDesk>> GetWorkingDesksAsync()
+        {
+            return await _workingDeskRepository.ListAsync();
+        }
     }
 }
---
+++ TelegramBotAPI/Controllers/WorkingDeskController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer;
+using BusinessLayer.Interfaces;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelegramBotAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkingDeskController : ControllerBase
+    {
+        private readonly IWorkingDeskService _workingDeskService;
+        private readonly ILogger<WorkingDeskController> _logger;
+        public WorkingDeskController(WorkingDeskService workingDeskService, ILogger<WorkingDeskController> logger)
+        {
+            _logger = logger;
+            _workingDeskService = workingDeskService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<WorkingDesk>>> GetWorkingDesksAsync()
+        {
+            return await _workingDeskService.GetWorkingDesksAsync();
+        }
+        
+
+        
+    }
+}
