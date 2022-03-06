d6a55d58f93ccdec835d6b26235b31fb9f50bff5
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2022-03-07 00:53:42 +0500

    some changes

--- DataLayer/Models/Reserve.cs
+++ DataLayer/Models/Reserve.cs
@@ -1,15 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
 
 namespace DataLayer.Models
 {
     public class Reserve : BaseEntity
     {
+        public int WorkingDeskId { get; set; }
+        [JsonIgnore]
+        public WorkingDesk? WorkingDesk { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
         public int UserId { get; set; }
+        [JsonIgnore]
+        public User? User { get; set; } 
 
         public int BookingTypeId { get; set; }
-        public BookingType BookingType { get; set; }
+        [JsonIgnore]
+        public BookingType? BookingType { get; set; }
+
+        public bool IsReccuring { get; set; }
+
         public int Frequency { get; set; }
     }
 }
